7991372b14011568dbaec0b35f391fa22163111b
Alexhandal <alexanderhandal12@gmail.com>
2022-03-28 13:45:19 -0400

    trying to make image switcher

--- my-project/main.js
+++ my-project/main.js
@@ -1,9 +1,1 @@
 import "./index.css";
-
-function myFunction(imgs) {
-  var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
-  expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
-  expandImg.parentElement.style.display = "block";
-}
